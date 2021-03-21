de05076c1422ed9334bbdd09ba356ea70224ee5c
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-21 18:48:37 +0300

    Changing the code file type Now the program will be written in C ++

--- attributes.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <fileapi.h>
-#include <AclAPI.h>
-#include <AccCtrl.h>
-
-void main(void) {
-	
-}
---
+++ attributes.cpp
@@ -0,0 +1,12 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+#include <stdio.h>
+#include <windows.h>
+#include <lm.h>
+#include <sddl.h>
+#include <AclAPI.h>
+
+int main(){
+	
+}
