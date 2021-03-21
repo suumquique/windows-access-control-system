de76c69ea59faaf0ad8dfbe8fde58c8e74eb7062
suumquique <lebedenko.fa@edu.spbstu.ru>
2021-03-21 22:20:17 +0300

    Modular project structure introduced The main file is divided into many separate ones containing functions grouped by their meaning. All header files, constants and libraries, as well as structures and descriptions of functions, are placed in the main.h file

---
+++ create.cpp
@@ -0,0 +1,70 @@
+#include "main.h"
+
+#define MAX_FILE_PATH_LEN 1000
+
+HANDLE createFileAndGetDescriptor() {
+	wchar_t fileName[MAX_FILE_PATH_LEN];
+	wprintf(L"Input a file name to create (with extension): ");
+	if (!wscanf(L"%s", fileName)) {
+		wprintf(L"Cannot read file name.\n");
+		exit(1);
+	}
+
+	SECURITY_DESCRIPTOR sd;
+	SECURITY_ATTRIBUTES sa;
+
+	DWORD dwErrCode; // код возврата
+ // инициализируем версию дескриптора безопасности
+	if (!InitializeSecurityDescriptor(
+		&sd,
+		SECURITY_DESCRIPTOR_REVISION))
+	{
+		dwErrCode = GetLastError();
+		printf("Initialize security descroptor failed.\n");
+		printf("Error code: %d\n", dwErrCode);
+		exit(dwErrCode);
+	}
+	// устанавливаем SID владельца объекта
+	if (!SetSecurityDescriptorOwner(
+		&sd, // адрес дескриптора безопасности
+		NULL, // не задаем владельца
+		SE_OWNER_DEFAULTED)) // определить владельца по умолчанию
+	{
+		dwErrCode = GetLastError();
+		perror("Set security descriptor owner failed.\n");
+		printf("The last error code: %u\n", dwErrCode);
+		exit(dwErrCode);
+	}
+	// устанавливаем SID первичной группы владельца
+	if (!SetSecurityDescriptorGroup(
+		&sd, // адрес дескриптора безопасности
+		NULL, // не задаем первичную группу
+		SE_GROUP_DEFAULTED)) // определить первичную группу по умолчанию
+	{
+		dwErrCode = GetLastError();
+		perror("Set security descriptor group failed.\n");
+		printf("The last error code: %u\n", dwErrCode);
+		exit(dwErrCode);
+	}
+	// проверяем структуру дескриптора безопасности
+	if (!IsValidSecurityDescriptor(&sd)) {
+		dwErrCode = GetLastError();
+		perror("Security descriptor is invalid.\n");
+		printf("The last error code: %u\n", dwErrCode);
+		exit(dwErrCode);
+	}
+	// инициализируем атрибуты безопасности
+	sa.nLength = sizeof(sa); // устанавливаем длину атрибутов защиты
+	sa.lpSecurityDescriptor = &sd; // устанавливаем адрес SD
+	sa.bInheritHandle = FALSE;
+
+	HANDLE fileHandle = CreateFileW(fileName, GENERIC_READ | GENERIC_WRITE, 0, &sa, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+	if (fileHandle == INVALID_HANDLE_VALUE) {
+		wprintf(L"Cannot create file with current security attributes.\n");
+		exit(1);
+	}
+
+	wprintf(L"File with specified security attributes was successfully created.\n\n\n");
+
+	return fileHandle;
+}
---
+++ main.h
@@ -0,0 +1,26 @@
+#ifndef UNICODE
+#define UNICODE
+#endif
+
+#ifndef MAIN_MODULE
+#define MAN_MODULE
+
+#pragma comment(lib, "Advapi32.lib")
+
+#include <stdio.h>
+#include <windows.h>
+#include <lm.h>
+#include <sddl.h>
+#include <AclAPI.h>
+#include <fileapi.h>
+
+typedef struct _account {
+	wchar_t* userName;
+	wchar_t* domainName;
+} Account;
+
+HANDLE createFileAndGetDescriptor();
+Account getUserAccountFromSID(PSID lpSID);
+void printFileSecurityInfo(HANDLE fileDescriptor);
+
+#endif // !MAIN_MODULE
